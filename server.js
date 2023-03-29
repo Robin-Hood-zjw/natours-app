fbd2f73a9b38e3e943081a939a2cb6c6a091ccdc
Robin-Hood-zjw <jiawenz@umich.edu>
2023-03-29 01:38:39 -0400

    update mongoose settings

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line no-unused-vars
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB, {
+  useNewUserParser: true,
+});
 
 const app = require('./app');
 
